baacb12f8e90357a22a25cc27ea4fd1a598cd7f9
natsmnv <natnatanatasha@gmail.com>
2023-03-19 15:08:23 +0200

    Add index and script files

---
+++ script.js
@@ -0,0 +1,9 @@
+let firstString = prompt('Enter first string');
+let secondString = prompt('Enter second string');
+let thirdString = prompt('Enter third string');
+let res = firstString + secondString + thirdString;
+console.log(res);
+
+let num = prompt('Enter five-digit number');
+let result = num.split("").join(" ");
+console.log(result);
